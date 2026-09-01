52f6e8f05e706a8a22d13f7df9c9d7e003ad8cff
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-01 12:48:41 +0500

    Implement /review/:isbn api in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,13 +41,23 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = Object.values(books).filter(book => book.title === title);
+  if (matchingBooks.length > 0) {
+    return res.status(200).json(matchingBooks);
+  }
+  return res.status(404).json({ message: "Book of title not found" });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    return res.status(200).json(book.reviews);
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
