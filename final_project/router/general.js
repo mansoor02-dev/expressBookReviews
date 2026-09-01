e0392ee0d17a5707ea24c16108e4631ee77ed4a1
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-01 11:54:00 +0500

    Implement /isbn/:isbn in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,13 +14,18 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books, null, 4));
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.send(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book Not Found"});
+  }
  });
   
 // Get book details based on author
