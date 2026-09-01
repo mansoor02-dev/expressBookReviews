b0c9476c2f32ac759accca591dcd706f1173994c
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-01 13:01:30 +0500

    Implement all the apis in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,7 +8,16 @@ const public_users = express.Router();
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  if (username && password) {
+    if (isValid(username)) {
+      users.push({'username': username, 'password': password})
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
+    } else {
+        return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Please enter username or password"});
 });
 
 // Get the book list available in the shop
